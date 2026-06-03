6f2607b16fb4a3e4b00acad1001a55bae8396337
Mrityunjay yadav <mrityunjayyadav2076@gmail.com>
2026-06-03 12:32:24 +0530

    Create Prime_Number.c

---
+++ Day-3/Prime_Number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) 
+    {
+        flag = 0;
+    } 
+    else 
+    {
+        for (i = 2; i <= num / 2; i++) 
+        {
+            if (num % i == 0) 
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if (flag == 1)
+        printf("%d is a Prime Number.\n", num);
+    else
+        printf("%d is not a Prime Number.\n", num);
+
+    return 0;
+}
