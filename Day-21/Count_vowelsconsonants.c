adf309b80da267525870dcb954f996aa9f3bc7fa
Mrityunjay yadav <mrityunjayyadav2076@gmail.com>
2026-06-26 12:40:34 +0530

    Create Count_vowelsconsonants.c

---
+++ Day-21/Count_vowelsconsonants.c
@@ -0,0 +1,33 @@
+#include <stdio.h>   
+#include <string.h>  
+
+int main() {
+    
+    char str[100];  
+    
+    int vowels = 0, consonants = 0;  
+
+    printf("Enter a string: ");
+     
+    gets(str);  
+
+    for (int i = 0; i < strlen(str); i++) {
+
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+                str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+
+                vowels++; 
+
+            } else {
+                
+                consonants++;  
+            }
+        }
+    }
+
+    printf("Vowels: %d\nConsonants: %d\n", vowels, consonants);
+
+    return 0;
+}
